b10cca18fa0103a2d4c1093cca26c32e7f92cca8
zxf <534193896@qq.com>
2016-02-17 20:30:53 +0800

    用户bean

---
+++ src/com/mod/beans/Person.java
@@ -0,0 +1,21 @@
+package com.mod.beans;
+
+public class Person {
+	//name
+	private String name;
+	//passWord
+	private String passWord;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	
+}
