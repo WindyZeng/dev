f6cf4e676c0f2c5fab7d8cdf3803d4d2ad0cd629
zxf <534193896@qq.com>
2016-03-03 17:59:39 +0800

    Built environment

---
+++ src/com/mod/beans/User.java
@@ -0,0 +1,50 @@
+package com.mod.beans;
+
+public class User {
+	private long uid;
+	private String loginname;
+	private String loginpass;
+	private String email;
+	private boolean status;
+	private String activationCode;
+	
+	
+	public long getUid() {
+		return uid;
+	}
+	public void setUid(long uid) {
+		this.uid = uid;
+	}
+	public String getLoginname() {
+		return loginname;
+	}
+	public void setLoginname(String loginname) {
+		this.loginname = loginname;
+	}
+	public String getLoginpass() {
+		return loginpass;
+	}
+	public void setLoginpass(String loginpass) {
+		this.loginpass = loginpass;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public String getActivationCode() {
+		return activationCode;
+	}
+	public void setActivationCode(String activationCode) {
+		this.activationCode = activationCode;
+	}
+	
+	
+}
